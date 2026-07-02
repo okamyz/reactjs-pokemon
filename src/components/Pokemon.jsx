6ca92e1eb4472d51969ddf1609685ff34aec5cf5
jaa <farezaahmadk@gmail.com>
2026-07-02 13:16:47 +0700

    Update Pokemon.jsx

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react"
 function Pokemon() {
     const [pokemonList, setPokemonList] = useState([])
+    const [loading, setLoading] = useState(true)
     async function getAllPokemon() {
         const apiUrl = "https://pokeapi.co/api/v2/pokemon";
         const resData = await fetch(apiUrl);
@@ -11,25 +12,28 @@ function Pokemon() {
             const resDataDetail = await fetch(item.url);
             const jsonDataDetail = await resDataDetail.json();  
             pokemonDetail[index] = jsonDataDetail
-            
+            setPokemonList([...pokemonDetail])
         })
-        // setPokemonList(jsonData.results);
-        console.log(pokemonDetail);
     }
 
     useEffect(() => {
         getAllPokemon();
+        setLoading(false)
     },[])
 
     // console.log(pokemonList);
     return (
         <div className='wrapper'>
             <div className="content">
+                {
+                    loading && (<div className='loading'>load all the pokemon...</div>)
+                }
                 <div className="grid">
                     {
-                        pokemonList.map((item)=>{
+                        pokemonList.map((item, index)=>{
                             return(
-                                <div className='item'>
+                                <div className='item' key={index}>
+                                    <div className='image'><img src={item.sprites.front_default} alt="" /></div>
                                     <div className='title'>{item.name}</div>
                                 </div>
                             )
